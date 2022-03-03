6904262f4e07d3293cb21fae851f85c57510e9e2
shittu adewale <sheriffhardewale@gmail.com>
2022-03-03 16:24:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Account from "components/Account/Account";
+import Chains from "components/Chains";
+import ERC20Balance from "components/ERC20Balance";
+import Wallet from "components/Wallet";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
+import NativeBalance from "components/NativeBalance";
+import "./style.css";
+import Contract from "components/Contract/Contract";
+import MenuItems from "./components/MenuItems";
+const { Header } = Layout;
+
+const styles = {
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "Roboto, sans-serif",
+    color: "#041836",
+    marginTop: "130px",
+    padding: "10px",
+  },
+  header: {
+    position: "fixed",
+    zIndex: 1,
+    width: "100%",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontFamily: "Roboto, sans-serif",
+    borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+    padding: "0 10px",
+    boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+  },
+  headerRight: {
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+    fontSize: "15px",
+    fontWeight: "600",
+  },
+};
+const App = ({ isServerInfo }) => {
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+    useMoralis();
+
+  useEffect(() => {
+    const connectorId = window.localStorage.getItem("connectorId");
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
+      enableWeb3({ provider: connectorId });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  return (
+    <Layout style={{ height: "100vh", overflow: "auto" }}>
+      <Router>
+        <Header style={styles.header}>
+          <MenuItems />
+          <div style={styles.headerRight}>
+            <Chains />
+            <NativeBalance />
+            <Account />
+          </div>
+        </Header>
+
+        <div style={styles.content}>
+          <Switch>
+            <Route isServerInfo={isServerInfo} path="/wallet">
+              <Wallet />
+            </Route>
+
+            <Route path="/erc20balance">
+              <ERC20Balance />
+            </Route>
+
+            <Route path="/contract">
+              <Contract />
+            </Route>
+
+            <Route path="/ethereum-boilerplate">
+              <Redirect to="/quickstart" />
+            </Route>
+            <Route path="/nonauthenticated">
+              <>Please login using the "Authenticate" button</>
+            </Route>
+          </Switch>
+
+          <div>hello world</div>
+        </div>
+      </Router>
+    </Layout>
+  );
+};
+
+export default App;
